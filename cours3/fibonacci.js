c905284f36ecc1e618a086d857df9c65292c1c06
David FABY <faby@IUTHAGUENAU.LAN>
2016-11-28 10:18:52 +0100

    Ajustements + ajout fibonacci recursive

---
+++ cours3/fibonacci.js
@@ -0,0 +1,21 @@
+function fibonacciNonRecursive(nb){
+  var n = 0,
+      m = n,
+      fibonacci = [];
+  for(var i = 0; i < nb; i++){
+    if(n > 0){
+      fibonacci.push(n);
+      n = n + m;
+      m = n - m;
+    } else {
+      fibonacci.push(n);
+      n = 1;
+    }
+  }
+  return fibonacci;
+}
+
+function fibonacciRecursive(num){
+  if (num <= 1) return 1;
+    return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
+}
