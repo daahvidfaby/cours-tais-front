71af79d71d3b3016f4b5260c5c073674a5cc3b4c
David FABY <faby@IUTHAGUENAU.LAN>
2016-11-28 11:13:39 +0100

    Utilisation de fonctions pour reproduire la suite de fibonacci (de manière non récursive et récursive)

--- cours3/fibonacci.js
+++ cours3/fibonacci.js
@@ -15,6 +15,7 @@ function fibonacciNonRecursive(nb){
   return fibonacci;
 }
 
+
 function fibonacciRecursive(num){
   if (num <= 1) return 1;
     return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
